b8ba6de5b030dd7df27de0cc1c07cc2901dab4e7
Daniel Velazco <225565708+danielalvc@users.noreply.github.com>
2025-08-09 17:22:41 +0200

    Unpacked from Spring Initializr with Spring Web and HttpClient

---
+++ src/main/java/com/danielalvc/public_holiday/PublicHolidayApplication.java
@@ -0,0 +1,13 @@
+package com.danielalvc.public_holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicHolidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicHolidayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielalvc/public_holiday/PublicHolidayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielalvc.public_holiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicHolidayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
